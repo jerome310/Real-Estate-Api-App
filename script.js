e6be2824038e49ba614530f6b16ecb67d2d578e0
jerome310 <jeromeyoung333@gmail.com>
2023-07-31 11:23:16 -0400

    made changes

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let loadPhone = document.querySelector(".load-phone");
 // Home Screen Delay Page
 function delay() {
     setTimeout( () => 
-    { location.href = "/signUp.html";}, 2000 
+    { location.href = "/signUp.html";}, 3000 
     );
 }
 
