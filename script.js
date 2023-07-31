05448b8d0251b95b1076a46d901b15e2129ea576
jerome310 <jeromeyoung333@gmail.com>
2023-07-31 01:42:56 -0400

    real estate app first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let loadPhone = document.querySelector(".load-phone");
+
+// Home Screen Delay Page
+function delay() {
+    setTimeout( () => 
+    { location.href = "/signUp.html";}, 2000 
+    );
+}
+
+delay();
