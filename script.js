997f752810bf729f2c0176e6b17fc48ec0ab926a
jerome310 <jeromeyoung333@gmail.com>
2023-08-06 16:52:24 -0400

    add login text

---
+++ script.js
@@ -0,0 +1,7 @@
+// Sign Up File
+let signUpBtn = document.querySelector(".sign-up-button");
+console.log(signUpBtn)
+
+signUpBtn.addEventListener('click', () => {
+    location.href = "/homepage.html";
+})
