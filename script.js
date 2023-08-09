91f6560b7e3a4424a29cebd37d298139bf8f68e4
jerome310 <jeromeyoung333@gmail.com>
2023-08-09 02:32:02 -0400

    created a feature on sign in page social media buttons to pop up with an alert message

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 // Sign Up File
 let signUpBtn = document.querySelector(".sign-up-button");
-console.log(signUpBtn)
+let btns = document.querySelectorAll('.sign-up-btns');
+console.log(btns)
 
 signUpBtn.addEventListener('click', () => {
     location.href = "/homepage.html";
 })
+
+// Sign In Buttons
+btns.forEach(btn => {
+  btn.addEventListener('click', () => {
+    alert('Please, fill out the input fields above')
+  })
+});
